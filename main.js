adc42a6d1dc7796d741e4c6c0e83fca3d7e9ba8e
suryarupHTML <83803411+suryarupHTML@users.noreply.github.com>
2022-01-24 23:35:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+function next() {
+    window.location = "plants.html";
+}
---
+++ plants.js
@@ -0,0 +1,15 @@
+function lotus() {
+    window.alert("Lotus is a beautiful flower that grows on the water surface of freshwater lakes and ponds. Lotus is the national flower of India. It is usually pink, white or blue in color and is really beautiful and mindcatching. The lotus flower is also known as Water Lilly")
+}
+function sunflower() {
+    window.alert("Sunflower is a very famous plant in the world. It is known for its bright yellow color and it always face the sun. The sunflowers are cultivated in open fields in large numbers which makes the scenario gorgeous. It is also used to make vegetable oil.")
+}
+function rose() {
+    window.alert("The rose flower is famous all over the world for its beauty. Roses are of many colours, mainly red, yellow, and pink. Rose Day is celebrated on 12th February every year. Roses also have medicinal uses.")
+}
+function rafflesia() {
+    window.alert("The flower with the world's largest bloom is the Rafflesia arnoldii. This rare flower is found in the rainforests of Indonesia. It can grow to be 3 feet across and weigh up to 15 pounds! It is a parasitic plant, with no visible leaves, roots, or stem.")
+}
+function pancy() {
+    window.alert("Pansies are a symbol of love and affectionate thoughts (their name is thought to derive from the French word for thought). In Shakespeare's A Midsummer Night's Dream, the juice of a pansy was used in a love potion, referencing the Ancient Greeks' and Celts' use of the flower in herbal remedies and love potions.")
+}
